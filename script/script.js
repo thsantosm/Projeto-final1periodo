4d1ff7bd4f8d269bced6cbfd83c26dbae8306abd
Thiago Santos <114359628+thsantosm@users.noreply.github.com>
2022-12-04 21:54:16 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// função para slide auto
+// let count = 1;
+// document.getElementById("slide1").checked = true;
+
+// setInterval( function(){
+//     nextImage();
+// }, 4000)
+
+// function nextImage(){
+//     count++;
+//     if(count>6){
+//       count = 1;
+//     }   
+//     document.getElementById("slide"+count).checked = true;
+// }
